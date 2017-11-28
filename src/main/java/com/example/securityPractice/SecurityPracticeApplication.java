dc2719347ec41d089225ff87ab2c721e7ab1d04e
hamilton moore <hamlmoor@gmail.com>
2017-11-28 12:39:49 -0600

    created html files

---
+++ src/main/java/com/example/securityPractice/SecurityPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.example.securityPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityPracticeApplication.class, args);
+	}
+}
